cde05461c26d235fa1a5b3de14d70ff41a4f1b33
Bogdan Klimoshenko <bklimoshenko@gmail.com>
2022-07-15 08:53:29 -0400

    Create Student and User entity classes, add Lombok as dependency

--- src/main/java/com/bklimoshemko/SpringCRUD/entity/Student.java
+++ src/main/java/com/bklimoshemko/SpringCRUD/entity/Student.java
@@ -1,4 +1,14 @@
 package com.bklimoshemko.SpringCRUD.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student")
 public class Student {
+    private int UUDI;
+    private String firstName;
+    private String lastName;
+
+
 }
