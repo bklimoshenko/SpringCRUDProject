c9bb0db54a95d206ede395a002561461b5700865
Bogdan Klimoshenko <bklimoshenko@gmail.com>
2022-07-18 15:42:54 -0400

    Added functionality to view students in the browser. Added ability to step through pages if the amount of students > DEFAULT_PAGE_SIZE

--- src/main/java/com/bklimoshemko/SpringCRUD/controller/StudentController.java
+++ src/main/java/com/bklimoshemko/SpringCRUD/controller/StudentController.java
@@ -1,18 +1,46 @@
 package com.bklimoshemko.SpringCRUD.controller;
 
+import com.bklimoshemko.SpringCRUD.entity.Student;
+import com.bklimoshemko.SpringCRUD.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
+
+    static final int DEFAULT_PAGE_SIZE = 10;
+
+    private final StudentService service;
+
+    @Autowired
+    public StudentController(final StudentService service){
+        this.service = service;
+    }
+
     @GetMapping("/students")
     public String index(){
         return("redirect:/list");
     }
 
     @GetMapping("/students/list")
-    public String getStudents(final Model model){
+    public String getStudents(final Model model,
+                              @RequestParam(value="page", defaultValue = "0") final int pageNumber,
+                              @RequestParam(value="size", defaultValue = DEFAULT_PAGE_SIZE + "") final int pageSize){
+
+        final Page<Student> studentPage = service.getStudent(pageNumber, pageSize);
+
+        final int currentPageNumber = studentPage.getNumber();
+        final int previousPageNumber = studentPage.hasPrevious() ? currentPageNumber - 1 : -1;
+        final int nextPageNumber = studentPage.hasNext() ? currentPageNumber + 1 : -1;
+
+        model.addAttribute("students", studentPage.getContent());
+        model.addAttribute("previousPageNumber", previousPageNumber);
+        model.addAttribute("nextPageNumber", nextPageNumber);
+
         return "students/list";
     }
 
